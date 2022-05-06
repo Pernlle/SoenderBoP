d8c98aa3e62817f1b9a3e5b8bad7e3924a3fc5b1
Pernlle <pkoe37782@edu.ucl.dk>
2022-05-06 13:12:31 +0200

    UNITTEST 1.2

--- UnitTesting/UnitTest1.cs
+++ UnitTesting/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace UnitTesting
         [TestMethod]
         public void Test1CRUDConnection()
         {
-            //Create kunde = new Create("","","","");
+            //Create kunde = new Create();
             //kunde.Setpostnummer = 1000;
             ////Assert.Pass(); //skal være rigtigt (pass=gør intet?)
             //Assert.AreEqual(1000, kunde.Getpostnummer);
