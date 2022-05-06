0c61bd9f0f49bf126fc023bfea1449710f514ab3
Fiemues <annesofiepoerksen@gmail.com>
2022-05-06 11:17:56 +0200

    login

--- SoenderBoP/Login.cs
+++ SoenderBoP/Login.cs
@@ -20,26 +20,27 @@ namespace SoenderBoP
         private void loginBtn_Click(object sender, EventArgs e)
         {
             //Login button
-            
-            //string id = idBox.Text;
-            //string pass = passBox.Text;
 
-            //// assumption: //double assumption dont assume anything
-            //bool id_ok = true, pass_ok = true;
+            string id = idBox.Text;
+            string pass = passBox.Text;
 
-            //// length check:
-            //if (id.Contains("1")) id_ok = true;
-            //if (pass.Contains("root")) pass_ok = true;
-                     
-            //if (id_ok && pass_ok)
-            //{
-            //    Forside.manuPanel.Controls.Clear();
-            //    AMenu frm = new AMenu() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
-            //    frm.FormBorderStyle = FormBorderStyle.None;
-            //    this.panel1.Controls.Add(frm);
-            //    frm.Show();
-            //}
-            //else { MessageBox.Show("Prøv igen du"); }
+            // assumption: //double assumption dont assume anything
+            bool id_ok = true, pass_ok = true;
+
+            // length check:
+            if (id.Contains("1")) id_ok = true;
+            if (pass.Contains("root")) pass_ok = true;
+
+            if (id_ok && pass_ok)
+            {
+                Forside fs = new Forside();
+                fs.manuPanel.Visible = true;
+                AMenu frm = new AMenu() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+                frm.FormBorderStyle = FormBorderStyle.None;
+                this.panel1.Controls.Add(frm);
+                frm.Show();
+            }
+            else { MessageBox.Show("Prøv igen du"); }
         }
     }
 }
