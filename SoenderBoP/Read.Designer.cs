0287baaf1256a47cc7857e66c3d441ea4ec7bed8
Fiemues <annesofiepoerksen@gmail.com>
2022-05-06 13:04:31 +0200

    read

--- SoenderBoP/Read.Designer.cs
+++ SoenderBoP/Read.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.readMedlemDGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.readMedlemDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // readMedlemDGV
+            // 
+            this.readMedlemDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.readMedlemDGV.Location = new System.Drawing.Point(27, 32);
+            this.readMedlemDGV.Name = "readMedlemDGV";
+            this.readMedlemDGV.RowHeadersWidth = 62;
+            this.readMedlemDGV.RowTemplate.Height = 28;
+            this.readMedlemDGV.Size = new System.Drawing.Size(743, 393);
+            this.readMedlemDGV.TabIndex = 0;
+            // 
+            // Read
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.readMedlemDGV);
+            this.Name = "Read";
             this.Text = "Read";
+            ((System.ComponentModel.ISupportInitialize)(this.readMedlemDGV)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView readMedlemDGV;
     }
 }
