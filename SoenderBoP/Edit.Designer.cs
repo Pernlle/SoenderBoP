48322d032b7751930cbb43c125ce883d3ddfbe1d
Pernlle <pkoe37782@edu.ucl.dk>
2022-05-09 10:51:28 +0200

    Edit 1.2

--- SoenderBoP/Edit.Designer.cs
+++ SoenderBoP/Edit.Designer.cs
@@ -31,23 +31,28 @@
             this.medlemDGV = new System.Windows.Forms.DataGridView();
             this.deleteBtn = new System.Windows.Forms.Button();
             this.updateBtn = new System.Windows.Forms.Button();
+            this.tlfMTxt = new System.Windows.Forms.MaskedTextBox();
+            this.emailMTxt = new System.Windows.Forms.MaskedTextBox();
+            this.navnTxt = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.medlemDGV)).BeginInit();
             this.SuspendLayout();
             // 
             // medlemDGV
             // 
             this.medlemDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.medlemDGV.Location = new System.Drawing.Point(12, 12);
+            this.medlemDGV.Location = new System.Drawing.Point(11, 10);
+            this.medlemDGV.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.medlemDGV.Name = "medlemDGV";
             this.medlemDGV.RowHeadersWidth = 62;
-            this.medlemDGV.Size = new System.Drawing.Size(490, 426);
+            this.medlemDGV.Size = new System.Drawing.Size(436, 341);
             this.medlemDGV.TabIndex = 0;
             // 
             // deleteBtn
             // 
-            this.deleteBtn.Location = new System.Drawing.Point(531, 12);
+            this.deleteBtn.Location = new System.Drawing.Point(474, 218);
+            this.deleteBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.deleteBtn.Name = "deleteBtn";
-            this.deleteBtn.Size = new System.Drawing.Size(159, 44);
+            this.deleteBtn.Size = new System.Drawing.Size(141, 35);
             this.deleteBtn.TabIndex = 1;
             this.deleteBtn.Text = "Slet";
             this.deleteBtn.UseVisualStyleBackColor = true;
@@ -55,26 +60,55 @@
             // 
             // updateBtn
             // 
-            this.updateBtn.Location = new System.Drawing.Point(531, 87);
+            this.updateBtn.Location = new System.Drawing.Point(474, 278);
+            this.updateBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.updateBtn.Name = "updateBtn";
-            this.updateBtn.Size = new System.Drawing.Size(159, 43);
+            this.updateBtn.Size = new System.Drawing.Size(141, 34);
             this.updateBtn.TabIndex = 2;
             this.updateBtn.Text = "Opdater";
             this.updateBtn.UseVisualStyleBackColor = true;
             this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
             // 
+            // tlfMTxt
+            // 
+            this.tlfMTxt.Location = new System.Drawing.Point(497, 102);
+            this.tlfMTxt.Name = "tlfMTxt";
+            this.tlfMTxt.Size = new System.Drawing.Size(100, 22);
+            this.tlfMTxt.TabIndex = 3;
+            // 
+            // emailMTxt
+            // 
+            this.emailMTxt.Location = new System.Drawing.Point(497, 155);
+            this.emailMTxt.Name = "emailMTxt";
+            this.emailMTxt.Size = new System.Drawing.Size(100, 22);
+            this.emailMTxt.TabIndex = 4;
+            // 
+            // navnTxt
+            // 
+            this.navnTxt.Location = new System.Drawing.Point(497, 50);
+            this.navnTxt.Name = "navnTxt";
+            this.navnTxt.ReadOnly = true;
+            this.navnTxt.Size = new System.Drawing.Size(100, 22);
+            this.navnTxt.TabIndex = 5;
+            this.navnTxt.TabStop = false;
+            // 
             // Edit
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(705, 450);
+            this.ClientSize = new System.Drawing.Size(627, 360);
+            this.Controls.Add(this.navnTxt);
+            this.Controls.Add(this.emailMTxt);
+            this.Controls.Add(this.tlfMTxt);
             this.Controls.Add(this.updateBtn);
             this.Controls.Add(this.deleteBtn);
             this.Controls.Add(this.medlemDGV);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Edit";
             this.Text = "Edit";
             ((System.ComponentModel.ISupportInitialize)(this.medlemDGV)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,5 +117,8 @@
         private System.Windows.Forms.DataGridView medlemDGV;
         private System.Windows.Forms.Button deleteBtn;
         private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.MaskedTextBox tlfMTxt;
+        private System.Windows.Forms.MaskedTextBox emailMTxt;
+        private System.Windows.Forms.TextBox navnTxt;
     }
 }
--- SoenderBoP/Edit.cs
+++ SoenderBoP/Edit.cs
@@ -19,12 +19,34 @@ namespace SoenderBoP
 
         private void deleteBtn_Click(object sender, EventArgs e)
         {
+            //hvilken tabel i db som skal arbejdes med
+            string insertInto = "Medlem";
+            // valgte medlem som skal slettes - valgt via dgv
+            string delete = ""; // dgv choice
 
+            CRUD.Delete(insertInto, delete);
         }
 
         private void updateBtn_Click(object sender, EventArgs e)
         {
+            //disse text vil blive udfyldt når man trykker på dgv og kan herefter kunne ændres, hvis man ønsker at opdatere dem (UPDATE KNAP)
+            int phone = Convert.ToInt32(emailMTxt.Text);
+            string email = emailMTxt.Text;
 
+            // Sætter values ind i en array, så de kan sendes over i metoderne (CRUD)
+            object[] data = { phone, email };
+
+            //hvilken tabel i db som skal arbejdes med
+            string insertInto = "Medlem";
+            // lav en add for hver parameter? så det kun er add der skal bruges ovre i create via foreach - genbrugelighed.
+            //Det er vigtigt at disse er adskildt med [,] og ikke [, ] og at de står i samme rækkefølge i både object, add og value.
+            string add = "tlf,email";
+            // lav en values add for hver value? så det kun er add der skal bruges ovre i create via foreach - genbrugelighed.
+            string values = "@tlf,@email";
+
+            string set = "tlf=@tlf, email=@email";
+            string where = "mId=@mId";
+            CRUD.Update(insertInto, add, set, where, values, data);
         }
     }
 }
