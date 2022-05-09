672804195591455c6c93ee00bb811cbdc62ffa00
Fiemues <annesofiepoerksen@gmail.com>
2022-05-09 11:41:55 +0200

    reservation

--- SoenderBoP/Reservation.Designer.cs
+++ SoenderBoP/Reservation.Designer.cs
@@ -29,49 +29,52 @@
         private void InitializeComponent()
         {
             this.reserveBtn = new System.Windows.Forms.Button();
-            this.loebeNrBox = new System.Windows.Forms.MaskedTextBox();
-            this.rId = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.loebeNrMTxt = new System.Windows.Forms.MaskedTextBox();
+            this.rIdMTxt = new System.Windows.Forms.MaskedTextBox();
+            this.datoSlutMTxt = new System.Windows.Forms.MaskedTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.datoStartMTxt = new System.Windows.Forms.MaskedTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // reserveBtn
             // 
-            this.reserveBtn.Location = new System.Drawing.Point(99, 192);
+            this.reserveBtn.Location = new System.Drawing.Point(95, 226);
             this.reserveBtn.Name = "reserveBtn";
-            this.reserveBtn.Size = new System.Drawing.Size(215, 32);
+            this.reserveBtn.Size = new System.Drawing.Size(219, 32);
             this.reserveBtn.TabIndex = 0;
             this.reserveBtn.Text = "Reserver";
             this.reserveBtn.UseVisualStyleBackColor = true;
             this.reserveBtn.Click += new System.EventHandler(this.reserveBtn_Click);
             // 
-            // loebeNrBox
+            // loebeNrMTxt
             // 
-            this.loebeNrBox.Location = new System.Drawing.Point(214, 62);
-            this.loebeNrBox.Name = "loebeNrBox";
-            this.loebeNrBox.Size = new System.Drawing.Size(100, 26);
-            this.loebeNrBox.TabIndex = 1;
+            this.loebeNrMTxt.Location = new System.Drawing.Point(213, 55);
+            this.loebeNrMTxt.Name = "loebeNrMTxt";
+            this.loebeNrMTxt.Size = new System.Drawing.Size(100, 26);
+            this.loebeNrMTxt.TabIndex = 1;
             // 
-            // rId
+            // rIdMTxt
             // 
-            this.rId.Location = new System.Drawing.Point(214, 94);
-            this.rId.Name = "rId";
-            this.rId.Size = new System.Drawing.Size(100, 26);
-            this.rId.TabIndex = 2;
+            this.rIdMTxt.Location = new System.Drawing.Point(213, 94);
+            this.rIdMTxt.Name = "rIdMTxt";
+            this.rIdMTxt.Size = new System.Drawing.Size(100, 26);
+            this.rIdMTxt.TabIndex = 2;
             // 
-            // maskedTextBox3
+            // datoSlutMTxt
             // 
-            this.maskedTextBox3.Location = new System.Drawing.Point(213, 136);
-            this.maskedTextBox3.Name = "maskedTextBox3";
-            this.maskedTextBox3.Size = new System.Drawing.Size(100, 26);
-            this.maskedTextBox3.TabIndex = 3;
+            this.datoSlutMTxt.Location = new System.Drawing.Point(213, 136);
+            this.datoSlutMTxt.Name = "datoSlutMTxt";
+            this.datoSlutMTxt.Size = new System.Drawing.Size(100, 26);
+            this.datoSlutMTxt.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(91, 67);
+            this.label1.Location = new System.Drawing.Point(91, 61);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 20);
             this.label1.TabIndex = 4;
@@ -89,24 +92,50 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(91, 136);
+            this.label3.Location = new System.Drawing.Point(91, 142);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.Size = new System.Drawing.Size(80, 20);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Dato";
+            this.label3.Text = "Dato start";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Dato slut";
+            // 
+            // datoStartMTxt
+            // 
+            this.datoStartMTxt.Location = new System.Drawing.Point(213, 176);
+            this.datoStartMTxt.Name = "datoStartMTxt";
+            this.datoStartMTxt.Size = new System.Drawing.Size(100, 26);
+            this.datoStartMTxt.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(53, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(296, 262);
+            this.panel1.TabIndex = 9;
             // 
             // Reservation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(477, 450);
+            this.Controls.Add(this.datoStartMTxt);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.maskedTextBox3);
-            this.Controls.Add(this.rId);
-            this.Controls.Add(this.loebeNrBox);
+            this.Controls.Add(this.datoSlutMTxt);
+            this.Controls.Add(this.rIdMTxt);
+            this.Controls.Add(this.loebeNrMTxt);
             this.Controls.Add(this.reserveBtn);
+            this.Controls.Add(this.panel1);
             this.Name = "Reservation";
             this.Text = "Reservation";
             this.ResumeLayout(false);
@@ -117,11 +146,14 @@
         #endregion
 
         private System.Windows.Forms.Button reserveBtn;
-        private System.Windows.Forms.MaskedTextBox loebeNrBox;
-        private System.Windows.Forms.MaskedTextBox rId;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.MaskedTextBox loebeNrMTxt;
+        private System.Windows.Forms.MaskedTextBox rIdMTxt;
+        private System.Windows.Forms.MaskedTextBox datoSlutMTxt;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox datoStartMTxt;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- SoenderBoP/Reservation.cs
+++ SoenderBoP/Reservation.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace SoenderBoP
 {
@@ -19,7 +20,38 @@ namespace SoenderBoP
 
         private void reserveBtn_Click(object sender, EventArgs e)
         {
+            
 
+            //hvilken tabel i db som skal arbejdes med
+            string insertInto = "Reserveret";
+            // lav en add for hver parameter? så det kun er add der skal bruges ovre i create via foreach - genbrugelighed.
+            //Det er vigtigt at disse er adskildt med [,] og ikke [, ] og at de står i samme rækkefølge i både object, add og value.
+            string add = "loebeNr,rId,datoStart,datoSlut";
+            // lav en values add for hver value? så det kun er add der skal bruges ovre i create via foreach - genbrugelighed.
+            string values = "@loebeNr,@rId,@,@dStart, @dSlut";
+
+            //Values fra tekstboxe:
+            string loebeNr = loebeNrMTxt.Text;
+            string rId = rIdMTxt.Text;
+            string datoStart = Convert.ToString(datoStartMTxt.Text);
+            string datoSlut = Convert.ToString(datoStartMTxt.Text);
+
+            string strconn = @"Server=den1.mssql7.gear.host; Database=soenderbodb; User ID=soenderbodb; Password=Ju7XZj_8pI2_";
+            //Sql Connection
+            SqlConnection conn = new SqlConnection(strconn);
+            string sqlCom = $"INSERT INTO {insertInto}({add}) VALUES ({values});";
+            //Sql Command
+            SqlCommand cmd = new SqlCommand(sqlCom, conn);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show($"{insertInto} oprettet");
+                //MessageBox.Show(sqlCom);
+            }
+            catch (Exception ecx) { MessageBox.Show(ecx.ToString()); }
         }
     }
 }
