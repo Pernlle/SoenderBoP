fc9dd0ed921165f1f0e484617045984633ee214a
Wimmercode <danielfunchwimmer@gmail.com>
2022-05-10 10:55:37 +0200

    Update Bolig.Designer.cs

--- SoenderBoP/Bolig.Designer.cs
+++ SoenderBoP/Bolig.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(23, 31);
+            this.label1.Location = new System.Drawing.Point(26, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(94, 39);
             this.label1.TabIndex = 0;
