d5661d37f13befba21c917dc45fe90007431634a
Pernlle <pkoe37782@edu.ucl.dk>
2022-05-10 12:21:49 +0200

    SEARCH 1.3 (new)

--- SoenderBoP/Search.Designer.cs
+++ SoenderBoP/Search.Designer.cs
@@ -40,11 +40,16 @@
             this.minKvmBox = new System.Windows.Forms.MaskedTextBox();
             this.maxKvmBox = new System.Windows.Forms.MaskedTextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // searchBtn
             // 
-            this.searchBtn.Location = new System.Drawing.Point(332, 193);
+            this.searchBtn.Location = new System.Drawing.Point(267, 210);
             this.searchBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.searchBtn.Name = "searchBtn";
             this.searchBtn.Size = new System.Drawing.Size(98, 43);
@@ -55,7 +60,7 @@
             // 
             // minPrisBox
             // 
-            this.minPrisBox.Location = new System.Drawing.Point(154, 90);
+            this.minPrisBox.Location = new System.Drawing.Point(89, 107);
             this.minPrisBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.minPrisBox.Name = "minPrisBox";
             this.minPrisBox.Size = new System.Drawing.Size(89, 22);
@@ -63,7 +68,7 @@
             // 
             // maxPrisBox
             // 
-            this.maxPrisBox.Location = new System.Drawing.Point(154, 143);
+            this.maxPrisBox.Location = new System.Drawing.Point(89, 160);
             this.maxPrisBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.maxPrisBox.Name = "maxPrisBox";
             this.maxPrisBox.Size = new System.Drawing.Size(89, 22);
@@ -72,7 +77,7 @@
             // Kvm
             // 
             this.Kvm.AutoSize = true;
-            this.Kvm.Location = new System.Drawing.Point(116, 220);
+            this.Kvm.Location = new System.Drawing.Point(51, 237);
             this.Kvm.Name = "Kvm";
             this.Kvm.Size = new System.Drawing.Size(33, 16);
             this.Kvm.TabIndex = 3;
@@ -81,7 +86,7 @@
             // minKvmLabel
             // 
             this.minKvmLabel.AutoSize = true;
-            this.minKvmLabel.Location = new System.Drawing.Point(116, 258);
+            this.minKvmLabel.Location = new System.Drawing.Point(51, 275);
             this.minKvmLabel.Name = "minKvmLabel";
             this.minKvmLabel.Size = new System.Drawing.Size(28, 16);
             this.minKvmLabel.TabIndex = 4;
@@ -90,7 +95,7 @@
             // maxKvmLabel
             // 
             this.maxKvmLabel.AutoSize = true;
-            this.maxKvmLabel.Location = new System.Drawing.Point(116, 307);
+            this.maxKvmLabel.Location = new System.Drawing.Point(51, 324);
             this.maxKvmLabel.Name = "maxKvmLabel";
             this.maxKvmLabel.Size = new System.Drawing.Size(32, 16);
             this.maxKvmLabel.TabIndex = 5;
@@ -99,7 +104,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(116, 143);
+            this.label1.Location = new System.Drawing.Point(51, 160);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 16);
             this.label1.TabIndex = 6;
@@ -108,7 +113,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(116, 93);
+            this.label2.Location = new System.Drawing.Point(51, 110);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(28, 16);
             this.label2.TabIndex = 7;
@@ -117,7 +122,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(116, 58);
+            this.label3.Location = new System.Drawing.Point(51, 75);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(75, 16);
             this.label3.TabIndex = 8;
@@ -125,7 +130,7 @@
             // 
             // minKvmBox
             // 
-            this.minKvmBox.Location = new System.Drawing.Point(154, 255);
+            this.minKvmBox.Location = new System.Drawing.Point(89, 272);
             this.minKvmBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.minKvmBox.Name = "minKvmBox";
             this.minKvmBox.Size = new System.Drawing.Size(89, 22);
@@ -133,7 +138,7 @@
             // 
             // maxKvmBox
             // 
-            this.maxKvmBox.Location = new System.Drawing.Point(154, 304);
+            this.maxKvmBox.Location = new System.Drawing.Point(89, 321);
             this.maxKvmBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.maxKvmBox.Name = "maxKvmBox";
             this.maxKvmBox.Size = new System.Drawing.Size(89, 22);
@@ -142,34 +147,64 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(565, 22);
+            this.label4.Location = new System.Drawing.Point(610, 24);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 16);
             this.label4.TabIndex = 11;
             this.label4.Text = "SØG BOLIG";
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1188, 67);
+            this.panel1.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.minPrisBox);
+            this.panel2.Controls.Add(this.searchBtn);
+            this.panel2.Controls.Add(this.maxKvmBox);
+            this.panel2.Controls.Add(this.maxPrisBox);
+            this.panel2.Controls.Add(this.minKvmBox);
+            this.panel2.Controls.Add(this.Kvm);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.minKvmLabel);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.maxKvmLabel);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 67);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(408, 483);
+            this.panel2.TabIndex = 15;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(408, 67);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(780, 483);
+            this.panel3.TabIndex = 16;
+            // 
             // Search
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 360);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.maxKvmBox);
-            this.Controls.Add(this.minKvmBox);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.maxKvmLabel);
-            this.Controls.Add(this.minKvmLabel);
-            this.Controls.Add(this.Kvm);
-            this.Controls.Add(this.maxPrisBox);
-            this.Controls.Add(this.minPrisBox);
-            this.Controls.Add(this.searchBtn);
+            this.ClientSize = new System.Drawing.Size(1188, 550);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Search";
             this.Text = "Search";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -187,5 +222,8 @@
         private System.Windows.Forms.MaskedTextBox minKvmBox;
         private System.Windows.Forms.MaskedTextBox maxKvmBox;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
