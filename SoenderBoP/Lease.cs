9ba46d55e5b8fcc264e39d15134636839843c9c7
Fiemues <annesofiepoerksen@gmail.com>
2022-05-11 11:03:12 +0200

    lejekontrakt

--- SoenderBoP/Lease.cs
+++ SoenderBoP/Lease.cs
@@ -16,5 +16,10 @@ namespace SoenderBoP
         {
             InitializeComponent();
         }
+
+        private void createLease_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
