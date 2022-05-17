7a748868f3e579c4854559259dfc2bfb9bfa21ac
Pernlle <pkoe37782@edu.ucl.dk>
2022-05-17 10:29:57 +0200

    Længde på dato Reservation

--- SoenderBoP/Reservation.cs
+++ SoenderBoP/Reservation.cs
@@ -20,7 +20,7 @@ namespace SoenderBoP
 
         private void reserveBtn_Click(object sender, EventArgs e)
         {
-            string strconn = @"Server=den1.mssql7.gear.host; Database=soenderbodb; User ID=soenderbodb; Password=Ju7XZj_8pI2_";
+            string strconn = @"Server=den1.mssql7.gear.host; Database=soenderbodb; User ID=soenderbodb; Password=password!";
             string loebeNr = this.loebeNr.Text;
             string rId = this.rId.Text;
 
@@ -36,31 +36,35 @@ namespace SoenderBoP
             string daSlutTime = dS1tartTime.Text;
             string daSlutMinut = ff.Text;
 
-            string dStart = daStartDag + "-" + daStartMaaned + "-" + daStartAar + "-" + daStartTime + ":" + daStartMinut;
-            string dSlut = daSlutDag + "-" + daSlutMaaned + "-" + daSlutAar + "-" + daSlutTime + ":" + daSlutMinut;
-
-            SqlConnection conn = new SqlConnection(strconn);
-
-            string sqlCom = "INSERT INTO Reserveret(loebeNr,rId,dStart,dSlut) VALUES (@loebeNr, @rId, @dStart, @dSlut);";
-            SqlCommand cmd = new SqlCommand(sqlCom, conn);
-            cmd.Parameters.Add("@loebeNr", System.Data.SqlDbType.VarChar);
-            cmd.Parameters["@loebeNr"].Value = Convert.ToString(loebeNr);
-            cmd.Parameters.Add("@rId", System.Data.SqlDbType.VarChar);
-            cmd.Parameters["@rId"].Value = Convert.ToString(rId);
-            cmd.Parameters.Add("@dStart", System.Data.SqlDbType.VarChar);
-            cmd.Parameters["@dStart"].Value = Convert.ToString(dStart);
-            cmd.Parameters.Add("@dSlut", System.Data.SqlDbType.VarChar);
-            cmd.Parameters["@dSlut"].Value = Convert.ToString(dSlut);
-
-            try
+            if (daStartDag.Length == 2 && daStartMaaned.Length == 2 && daStartTime.Length == 2 && daStartMinut.Length == 2 && daSlutDag.Length == 2 && daSlutMaaned.Length == 2 && daSlutTime.Length == 2 && daSlutMinut.Length == 2 && daStartAar.Length == 4 && daSlutAar.Length == 4)
             {
-                conn.Open();
-                cmd.ExecuteNonQuery();
-                conn.Close();
-                MessageBox.Show("Reserveret");
-                //MessageBox.Show(sqlCom);
+
+                string dStart = daStartDag + "-" + daStartMaaned + "-" + daStartAar + "-" + daStartTime + ":" + daStartMinut;
+                string dSlut = daSlutDag + "-" + daSlutMaaned + "-" + daSlutAar + "-" + daSlutTime + ":" + daSlutMinut;
+
+                SqlConnection conn = new SqlConnection(strconn);
+
+                string sqlCom = "INSERT INTO Reserveret(loebeNr,rId,dStart,dSlut) VALUES (@loebeNr, @rId, @dStart, @dSlut);";
+                SqlCommand cmd = new SqlCommand(sqlCom, conn);
+                cmd.Parameters.Add("@loebeNr", System.Data.SqlDbType.VarChar);
+                cmd.Parameters["@loebeNr"].Value = Convert.ToString(loebeNr);
+                cmd.Parameters.Add("@rId", System.Data.SqlDbType.VarChar);
+                cmd.Parameters["@rId"].Value = Convert.ToString(rId);
+                cmd.Parameters.Add("@dStart", System.Data.SqlDbType.VarChar);
+                cmd.Parameters["@dStart"].Value = Convert.ToString(dStart);
+                cmd.Parameters.Add("@dSlut", System.Data.SqlDbType.VarChar);
+                cmd.Parameters["@dSlut"].Value = Convert.ToString(dSlut);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("Reserveret");
+                    //MessageBox.Show(sqlCom);
+                }
+                catch (Exception ecx) { MessageBox.Show(ecx.ToString()); }
             }
-            catch (Exception ecx) { MessageBox.Show(ecx.ToString()); }
         }
     }
 }
