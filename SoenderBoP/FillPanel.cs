93b79d581c968ddfd078c7c291cc59d227327be3
Fiemues <annesofiepoerksen@gmail.com>
2022-05-19 11:23:44 +0200

    Maybe or maybe not

---
+++ SoenderBoP/FillPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoenderBoP
+{
+    internal class FillPanel
+    {
+
+        //panel1.Controls.Clear();
+        //    Login frm = new Login() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+        //frm.FormBorderStyle = FormBorderStyle.None;
+
+        //    this.panel1.Controls.Add(frm);
+
+        //    frm.Show();
+    }
+}
