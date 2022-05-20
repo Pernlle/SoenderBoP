f2207813b86d422c195bc642d21e202104068c6e
Pernlle <pkoe37782@edu.ucl.dk>
2022-05-20 10:40:04 +0200

    Waitlist_Print 2.1

---
+++ SoenderBoP/Print.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Data;
+
+
+namespace SoenderBoP
+{
+    internal class Print
+    {
+        public static void PrintIt(DataGridView dgv, string writerName, string[] headersarr)
+        {
+            string headers = "";
+            for (int i = 0; i < headersarr.Length; i++)
+            {
+                headers += PadBoth(headersarr[i], 5) + " | ";
+            }
+
+            TextWriter writer = new StreamWriter($@"..\..\..\SoenderBoP\Resources\{writerName}.Txt");
+            writer.WriteLine(headers);
+            for (int i = 0; i < dgv.Rows.Count - 1; i++) // rows
+            {
+                for (int j = 0; j < dgv.Columns.Count; j++) // columns
+                {
+                    if (j == dgv.Columns.Count - 1) // if last column
+                    {
+                        writer.WriteLine(PadBoth(dgv.Rows[i].Cells[j].Value.ToString(), 5));
+                    }
+                    else
+                        writer.Write(PadBoth(dgv.Rows[i].Cells[j].Value.ToString(), 5) + "|");
+                }
+            }
+            writer.Close();
+            MessageBox.Show("Exported");
+        }
+        public static string PadBoth(string source, int length)
+        {
+            int spaces = length - source.Length;
+            int padLeft = spaces / 2 + source.Length;
+            return source.PadLeft(padLeft).PadRight(length);
+
+        }
+    }
+}
