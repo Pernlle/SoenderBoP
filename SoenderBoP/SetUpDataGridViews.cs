406b40f71482c33e6f7f1b0fbe2ce6c0beb57e86
Pernlle <pkoe37782@edu.ucl.dk>
2022-05-27 11:46:02 +0200

    SETUP DGV SHOW METODE

---
+++ SoenderBoP/SetUpDataGridViews.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SoenderBoP
+{
+    internal class SetUpDataGridViews
+    {
+        public static DataGridView SetUpDGV(DataGridView dgvSource, string sqlcom)
+        {
+            DataGridView dgvView = dgvSource;
+            dgvView.DataSource = FillDataSource.GetDataSource(sqlcom);
+            DataGridView DGV = dgvView;
+            GetDGVStyle.GetStyle(DGV);
+            return dgvView;
+        }
+    }
+}
