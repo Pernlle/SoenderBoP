c59628f5b31acf52f4255d2ac17038a32dc4be0e
Pernlle <pkoe37782@edu.ucl.dk>
2022-06-01 10:21:24 +0200

    Commentary observerPattern

--- SoenderBoP/ObserverPattern.cs
+++ SoenderBoP/ObserverPattern.cs
@@ -3,30 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SoenderBoP
 {
     public class ObserverPattern
     {
-        // private observers array
+        // private observers array/list/stack som skal kunne opdateres
         private static List<IObservable> observers = new List<IObservable>();
 
-        //observe(observer: IObservable)
-        // observers.push(observer)
+        //observe(observer) -Tilføj en observer til listen over observere
+        //   observers.push(observer)
         public static void Observe(IObservable observer)
         {
             observers.Add(observer);
         }
 
-        //unobserve
-        // observers.findAndDestroy(observer )
+        //unobserve -fjern medlemmer fra observer liste.
+        //   observers.findAndDestroy(observer )
         public static void UnObserve(IObservable observer)
         {
             observers.Remove(observer);
         }
 
-        //notify 
-        // observers.foreach((observer) => observer.notify('Platanalle 47'))
+        //notify -Giv observere besked om eventuelle ændringer
+        //   observers.foreach((observer) => observer.notify('Platanalle 47'))
         public static void Notify()
         {
             foreach (IObservable observer in observers)
@@ -39,14 +40,15 @@ namespace SoenderBoP
     //interface, 'IObservable', som indeholder en notify;
     public interface IObservable
     {
-        void notify(string email);
+        void notify(string message);
     }
 
     public class Medlem : IObservable //kan hedde alt :)
     {
-        public void notify(string email)
+        public void notify(string message)
         {
             //Noget 
+            MessageBox.Show(message);
         }
     }
 }
